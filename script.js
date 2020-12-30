2ebc348a7ed405b060dc99f576f306586d3508d2
Kelly Cook <kellyrcook1980@gmail.com>
2020-12-30 13:06:09 -0700

    updated settings and added dinner options

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-var dinnerArray = ["Spaghetti", "Chicken Enchiladas", "Fajitas", "Shepard's Pie", "Raspberry Chicken", "TaterTot Casserole", "Chicken Alfredo", "Chicken Noodle Casserole", "Texas Dinner", "Red Beans and Rice", "Meatloaf", "Tacos", "Fried Chicken", "Lasagna", "Pizza", "French Dip", ""  ];
+var dinnerArray = ["Spaghetti", "Chicken Enchiladas", "Fajitas", "Shepard's Pie", "Raspberry Chicken", "TaterTot Casserole", "Chicken Alfredo", "Chicken Noodle Casserole", "Texas Dinner", "Red Beans and Rice", "Meatloaf", "Tacos", "Fried Chicken", "Lasagna", "Pizza", "French Dip", "Philly Cheese steak", "Pancakes", "Breakfast in a Pan", "Chicken Caesar", "Steak and Potatoes", "Fish and Veggies", "Mac & Cheese", "Biscuits and Gravy", "Roast and Veggies", "Hamburgers/Dogs", "Kebabs", "Kolaches", "Sloppy Joes", "Stir Fry", "Chicken Curry", "Fish-heads and Rice", "Stroganoff"];
+
 
 function dinner () {
-  var food = dinnerArray[Math.floor(Math.random()*dinnerArray.length)];
+  var food = dinnerArray[Math.floor(Math.random()*dinnerArray.length-1)];
     document.querySelector("#display").textContent = food;
 }
 
